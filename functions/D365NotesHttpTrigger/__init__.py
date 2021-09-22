3906f689f707876f70e156bae58f9f8c9154bebf
Juan Pablo Olguin <olguin@intalio.com>
2021-09-22 12:46:14 -0300

    refactor out notes_trigger_function class and add unit tests. Fix deploy.yml to avoid using azure credentials and run unit tests

--- functions/D365NotesHttpTrigger/__init__.py
+++ functions/D365NotesHttpTrigger/__init__.py
@@ -1,14 +1,10 @@
-from requests.models import Response
-from shared_code.configuration_file import ConfigurationFile
+from D365NotesHttpTrigger.notes_trigger_function import NotesTriggerFunction
+
 import azure.functions as func
-import requests
 import json
 import os
 import logging
-import datetime
 
-
-from D365NotesHttpTrigger.note import Note
 from shared_code.oauth_client import OauthClient
 
 
@@ -17,7 +13,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     req_body = req.get_json()
     note_id = req_body["PrimaryEntityId"]
     crm_organization_URI = None
-    logging.info(f"ENTERING WITH NOTE_ID!!!!!!!!!!!!!!!!!!!!!!!!:{note_id} 2002")
+    logging.info(f"ENTERING Notes Trigger WITH NOTE_ID:{note_id} ")
 
     try:
        
@@ -35,57 +31,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
         oauth_token = crm_auth_token_result.json()["token"]
         
-        return copy_note_attachment_to_blob_container(crm_organization_URI, note_id, oauth_token)
+        return NotesTriggerFunction.copy_note_attachment_to_blob_container(crm_organization_URI, note_id, oauth_token)
     except Exception as ex:
         logging.error(f"Error in Note Http Trigger init method - EXCEPTION:{ex}")
         func.HttpResponse(json.dumps(f"Error in Note Http Trigger:{ex}", ensure_ascii=False),
                           mimetype="application/json",  status_code=401)
 
-
-def copy_note_attachment_to_blob_container(crm_organization_URI:str, note_id:str, oauth_token:str) -> Response:
-    try:
-        storage_account_name = os.environ["STORAGE_ACCOUNT_NAME"]
-        storage_container = os.environ["STORAGE_CONTAINER"]
-        crm_request_headers = {
-            'Authorization': 'Bearer ' + oauth_token,
-            'OData-MaxVersion': '4.0',
-            'OData-Version': '4.0',
-            'Accept': 'application/json',
-            'Content-Type': 'application/json; charset=utf-8',
-            'Prefer': 'odata.maxpagesize=500',
-            'Prefer': 'odata.include-annotations=OData.Community.Display.V1.FormattedValue'
-        }
-
-        file_request_headers = {
-           'Authorization': 'Bearer ' + oauth_token,
-           'Content-Type': 'application/octet-stream',
-        }
-
-        # full path to D365 Field Service REST api endpoint
-        rest_api_URL: str = crm_organization_URI+'/api/data/v9.1'
-
-        metadata_tags_values = {}
-
-        if(oauth_token != ''):
-            note: Note = Note.find(
-                rest_api_URL, crm_request_headers, note_id)
-
-        if note is not None:
-            if note.has_video_attachment():         
-                configuration_file = ConfigurationFile.load(storage_account_name, storage_container, os.environ["CONFIG_FILE"])          
-                metadata_tags_values = configuration_file.get_metadata_values(crm_organization_URI, crm_request_headers,  note.asdict())
-                note_video_content = note.download_attachment(rest_api_URL, file_request_headers)
-                if note_video_content is None:
-                   raise RuntimeError("Error downloading attachment.")
-
-                note.upload_attachment_to_container(note_video_content, metadata_tags_values, storage_account_name, storage_container, os.environ["AUTH_TOKEN_ENDPOINT"])
-
-       
-        return func.HttpResponse(json.dumps(metadata_tags_values, ensure_ascii=False), mimetype="application/json",  status_code=200)
-    except Exception as ex:
-        error_message = f"Error on Http Note Trigger: {ex}"
-        logging.error(error_message)
-        return func.HttpResponse(json.dumps(error_message, ensure_ascii=False), mimetype="application/json",  status_code=400)
 
 
 
