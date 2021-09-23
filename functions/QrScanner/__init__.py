c9c9ad3007f57d3b1ea9c5ea34c38a1de561e2b4
Juan Pablo Olguin <olguin@intalio.com>
2021-09-23 15:06:47 -0300

    try codeql analysis

--- functions/QrScanner/__init__.py
+++ functions/QrScanner/__init__.py
@@ -7,6 +7,7 @@ from shared_code.configuration_file import ConfigurationFile as ConfigurationFil
 
 from datetime import datetime
 
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         json_data = json.dumps(req.get_json())
@@ -14,7 +15,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         video_file_path = values[0]["data"]["video_file_path"]
         record_id = values[0]["recordId"]
         config = ConfigurationFile.get_json_file_from_url(os.path.dirname(video_file_path))
-        return func.HttpResponse(json.dumps(extract_barcodes(video_file_path, record_id, config),ensure_ascii=False), mimetype="application/json")
+        return func.HttpResponse(json.dumps(extract_barcodes(video_file_path, record_id, config), ensure_ascii=False), mimetype="application/json")
     except Exception:
         return func.HttpResponse(traceback.format_exc())
-
